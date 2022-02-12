e75d93b114b84dfa3756af4b93f84cd0dc264df2
Shashikumar <shashirgowdas67@gmail.com>
2022-02-12 12:43:27 +0530

    Added:calculate to find harmonic number

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program2
+{
+    class Program4
+    {
+        public static void HarmonicNumber()
+        {
+            Console.WriteLine("enter number");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double j = 0;
+            for (double i = 1; i <= n; i++)
+            {
+                j = j + 1 / i;
+                Console.WriteLine(j);
+
+            }
+
+        }
+    }
+}
