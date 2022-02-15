b5f1ed8fc61fc91d19aa72a3b8f67e27cf4da808
Shashikumar <shashirgowdas67@gmail.com>
2022-02-15 20:33:40 +0530

    Added:code to calculate factor

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program2
+{
+    class Program5
+    {
+        public static void Factors()
+        {
+            Console.WriteLine("Please enter your integer: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i * i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    n = n / i;
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
