7f5da7ed61c47fe8f387279de7e9a5d6f2705042
Shashikumar <shashirgowdas67@gmail.com>
2022-02-15 20:38:07 +0530

    Added:to calculate to power exponential in the program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program2
+{
+    class Program3
+    {
+        public static void ToFindPower()
+        {
+
+            int b = 2;
+            Console.WriteLine("Give the Exponent : ");
+            int e = int.Parse(Console.ReadLine());
+            if (0 <= e && e < 31)
+            {
+                Console.WriteLine("int e is overflow");
+            }
+            double Result = Math.Pow(b, e);
+            Console.WriteLine("Result : {0}", Result);
+            Console.ReadLine();
+
+        }
+    }
+}
