c3fca2cd46031711f4b50719c7092aedaec554a9
Shashikumar <shashirgowdas67@gmail.com>
2022-03-10 12:35:56 +0530

    Added:Create board

---
+++ Desktop/New folder (3)/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void CreateEmptyBoard()
+        {
+            Char[] board = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            CreateEmptyBoard();
+
+        }
+    }
+}
